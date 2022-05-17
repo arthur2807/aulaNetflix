c12962651ae60c051e5691c0021535133a62709b
arthur2807 <arthur.vinicius.cavalcanti@hotmail.com>
2022-05-17 15:02:14 -0300

    commit inicial aulaNetflix

---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    items:1,
+    merge:true,
+    loop:true,
+    margin:10,
+    video:true,
+    lazyLoad:true,
+    center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
